8f9df7bc280a78fb7f107c5d933b6da8dd387fe7
T R Methu <fmethubd@gmail.com>
2023-02-10 15:17:24 +0600

    adding js

--- js/login-form.js
+++ js/login-form.js
@@ -1,4 +1,23 @@
 const loginButton = document.getElementById ('btnLogin');
-loginButton.setAttribute('title', 'Hello, adding JavaScript to interactive');
+loginButton.addEventListener('click', functionOne);
+const warningText = document.getElementById('warnings');
+warningText.style.fontSize = '10px';
+warningText.style.color = 'white';
+
+const inputUserName = document.getElementById('inputUserName');
+const inputPassword = document.getElementById ('inputPassword');
+
+const userName = 'methu';
+const userPassword = '5252';
+
+function functionOne(){
+    if (inputUserName.value === userName || inputPassword === userPassword){
+        warningText.innerText = 'Successfully Working!';
+    }
+    else{
+        warningText.innerText = 'Something is missing!';
+    }
+    
+}
 
 
